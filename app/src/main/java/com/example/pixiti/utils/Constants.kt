d87f528021f757bc0435fe24c85fb140f3eecdac
M.Emre YILMAZ <memreyilmaz@gmail.com>
2021-02-28 08:49:02 +0300

    Update Constants.kt

--- app/src/main/java/com/example/pixiti/utils/Constants.kt
+++ app/src/main/java/com/example/pixiti/utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.pixiti.utils
 
 const val BASE_URL = "https://pixabay.com/api/"
-const val KEY = "?key=11868336-ddbb35d81dbcb178d2e654d7d"
+const val KEY = ""
 const val APP_NAME = "pixiti"
 const val TIMEOUT_INMILIS = 60000L
 const val PREFS_FILE = "com.example.pixiti.prefs"
